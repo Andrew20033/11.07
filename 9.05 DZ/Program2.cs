ff286520e7292d8ebbb9dd4b447e00be9bf7ea51
Stuk_uc23@student.itstep.org <Stuk_uc23@student.itstep.org>
2022-08-14 15:19:15 +0300

    Добавьте файлы проекта.

---
+++ 9.05 DZ/Program2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DZ_CSharp_11._07
+{
+
+
+    public class Number
+    {
+        double a;
+        public Number()
+        { }
+        public Number(double x)
+        { a = x; }
+        public void Print()
+        {
+            Console.WriteLine("a={0}", a);
+        }
+        public bool Prime()
+        {
+            bool resultprime = true;
+
+            if (a > 1)
+            {
+                for (var i = 2u; i < a; i++)
+                {
+                    if (a % i == 0)
+                    {
+                        resultprime = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                resultprime = false;
+            }
+
+            return resultprime;
+        }
+
+
+
+        public bool Correct()
+        {
+            bool resultcorrect = false;
+            if (a > 0)
+                resultcorrect = true;
+            return resultcorrect;
+        }
+
+
+    }
+
+}
